378e84a7a5c31b19c5279e9d0afff8b5bce135a1
Guilherme <guilhermebauer16@gmail.com>
2023-06-22 18:35:46 -0300

    Add connection

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import pymysql 
 
+connection = pymysql.connect(
+
+    host= '127.0.0.1' ,
+    port= 3306 ,
+    user= 'root' ,
+    password= '0910'
+)
+
+cursor = connection.cursor()
+
 print('Agenda de contatos ')
