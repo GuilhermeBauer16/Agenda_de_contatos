3e380f095c698d842cf24f2cfab2d33c4b9060bc
Guilherme <guilhermebauer16@gmail.com>
2023-06-23 18:51:01 -0300

    Add função de novo registro

---
+++ functions.py
@@ -0,0 +1,29 @@
+def converson_numero(msg,tipo= int):
+    while True:
+        numero = input(msg)
+
+        try:
+            num_convertido = tipo(numero) 
+            break
+
+        except ValueError:
+            print('\033[31mPor favor digite um número\033[m')
+
+
+def titulo(msg, num=50):
+    quant = num * 2
+    print('==' * num)
+    print(f'{msg:^{quant}}')
+    print('=='* num)
+
+
+def limpa_terminal():
+    import os 
+
+    if os.name == 'nt':
+        os.system('cls')
+
+    else:
+        os.system('clear')
+
+    
--- main.py
+++ main.py
@@ -1,13 +1,58 @@
-import pymysql 
+import mysql.connector
+import functions
+config = {
 
-connection = pymysql.connect(
+    'host': "127.0.0.1" ,
+    'port': "3306" ,
+    'user': "root" ,
+    'password': "0910",
+    'database': 'usuarios',
+    'raise_on_warnings': True
 
-    host= '127.0.0.1' ,
-    port= 3306 ,
-    user= 'root' ,
-    password= '0910'
-)
-
+}
+connection = mysql.connector.connect(**config)
 cursor = connection.cursor()
+senha = ''
 
-print('Agenda de contatos ')
+banco_usuarios = """CREATE DATABASE IF NOT EXISTS usuarios
+                    DEFAULT CHARACTER SET "utf8'
+                    DEFAULT COLLATE utf8_general_ci"""
+def regista():
+    global cursor
+    global senha 
+    nome = input('Digite seu nome: ')
+    senha = input('Digite sua senha: ')
+    try :
+        dados = f'''CREATE DATABASE IF NOT EXISTS {nome.replace(' ','_')}
+                DEFAULT CHARACTER SET "utf8'
+                DEFAULT COLLATE utf8_general_ci'''
+        cursor.execute(dados)
+        cursor.execute(f'''INSERT INTO usuarios ''')
+    except mysql.connector.Error as erro:
+        print( f'erro {erro}')
+        print('ouve um erro para registar o usuario ')
+
+
+
+# user = input('Digite o nome do usuario: ').upper()
+# password = input('Digite sua senha: ')
+# print('Agenda de contatos ')
+
+while True:
+    functions.titulo('Login')
+    print("""
+
+[1]Registar usuario 
+[2]Login
+[3]Sair 
+""")
+    print('==' * 50)
+    opcao = functions.converson_numero('Sua opção: ', int )
+    print('==' * 50)
+
+    if opcao == 1: 
+        regista()
+
+#fecha o banco de dados 
+connection.close()
+cursor.close()
