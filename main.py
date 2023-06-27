e90183c6f8a55ffb08cdf82162c2db52b6b19bb2
Guilherme <guilhermebauer16@gmail.com>
2023-06-26 22:07:42 -0300

    add função de adicionar novo contato

--- main.py
+++ main.py
@@ -22,9 +22,11 @@ def registra():
     senha = str(input('Digite sua senha: '))
 
     try :
+
         dados = f'''CREATE DATABASE IF NOT EXISTS {nome.replace(' ','_')}
                 DEFAULT CHARACTER SET 'utf8mb4'
                 DEFAULT COLLATE utf8mb4_general_ci'''
+        
         cursor.execute(dados)
         cursor.execute(f'''
                        CREATE USER "{nome.replace(' ', '_')}"@"localhost" 
@@ -42,28 +44,7 @@ def login():
 
     global cursor
     global connection
-    nome = str(input('Digite seu nome: '))
-    senha = str(input('Digite sua senha: '))
 
-    try: 
-        
-        cursor.execute(f'''USE {nome.replace(' ', '_')}''')
-        cursor.execute(f'''SELECT COUNT(*) FROM mysql.user WHERE user = %s 
-                       AND local = %s 
-                       AND authentication_string = %s''',
-                       (f'{nome.replace(' ', '_')}@localhost' , 'localhost', senha))
-        result = cursor.fetchone()
-
-        if result[0] == 1:
-            print(f'Login bem sucedido, seja bem vindo {nome.replace('_', ' ')}')
-        
-        else :
-            erros = mysql.connector.Error 
-            print(erros)
-            print('\033[31mUsuario ou senha incorretos\033[m')
-
-    except mysql.connector.Error as erro:
-        print(f'ocorreu o erro{erro}')
 
 
 while True:
@@ -82,8 +63,61 @@ while True:
         registra()
 
     elif opcao == 2:
-        login()
-    
+        while True:
+            nome = str(input('Digite seu nome: '))
+            senha = str(input('Digite sua senha: '))
+
+            try: 
+                config = {f'''
+
+            'host': "localhost" ,
+            'port': "3306" ,
+            'user': "{nome}" ,
+            'password': "{senha}",
+            'raise_on_warnings': True
+                '''}
+                cursor.execute(f"SHOW GRANTS FOR '{nome.replace(' ','_')}'@'localhost'")
+                resulta = cursor.fetchall()
+                print(resulta)
+                break
+                # cursor.execute(f'''SELECT User , authentication_string FROM mysql.user WHERE user = %s 
+                #                AND authentication_string = %s''',
+                #                (f'{nome.replace(' ', '_')}@localhost' , senha))
+                # result = cursor.fetchone()
+                # print(result)
+                # if result is not None:
+                #     print(f'Login bem sucedido, seja bem vindo {nome.replace('_', ' ')}')
+                
+                # else :
+        
+                #     print('\033[31mUsuario ou senha incorretos\033[m')
+
+            except mysql.connector.Error as erro:
+                print(f'ocorreu o erro{erro}')
+                print('\033[31mUsuario ou senha incorretos\033[m')
+
+        while True:
+            functions.titulo("Funções")
+            print('''
+    [1]Novo Contato
+    [2]Editar Contato
+    [3]Deletar Contato
+    [4]Sair 
+                ''')
+            print('==' * 50)
+            opcao_contato = functions.converson_numero('Sua opção: ', int)
+            cursor.execute(f'''USE {nome.replace(' ', '_')}''')
+            if opcao_contato == 1:
+                functions.titulo("Novo contato ")
+                nome_contato = str(input('Nome: '))
+                telefone = str('Telefone: ')
+                email = str(input('email: '))
+
+                tabela =  f"""CREATE TABLE IF NOT EXISTS {nome_contato.replace(' ','_')}
+                            id INT NOT NULL AUTO_INCREMENT ,
+                            nome VARCHAR(150) NOT NULL,
+                            telefone """
+
     elif opcao == 3:
         print('saindo...')
         sleep(1)
